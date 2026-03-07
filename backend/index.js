d0771a15e6ef13a10301df27c3c617ddaf03fc29
Pratyush Raj <143428765+pratyush12raj@users.noreply.github.com>
2026-03-07 14:15:44 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,31 +1,69 @@
 
-import express from "express"
-import dotenv from 'dotenv'
-import mongoose from 'mongoose'
-import noteroute from './route/note.route.js'
-import cors from "cors"
+// import express from "express"
+// import dotenv from 'dotenv'
+// import mongoose from 'mongoose'
+// import noteroute from './route/note.route.js'
+// import cors from "cors"
 
+
+// dotenv.config()
+
+// const app = express()
+// app.use(express.json())
+// app.use(cors())
+// try{
+   
+//     mongoose.connect(process.env.MONGO_URL)
+//     console.log("connected to Database")
+// }
+// catch(error){
+//     console.log("ERROR ", error)
+
+// }
+
+// app.use("/noteapp",noteroute)
+
+
+
+
+// app.listen(3000 , () => {
+//   console.log("Server running on port 3000")
+// })  
+
+
+
+
+
+
+
+
+
+import express from "express"
+import dotenv from "dotenv"
+import mongoose from "mongoose"
+import noteroute from "./route/note.route.js"
+import cors from "cors"
 
 dotenv.config()
 
 const app = express()
+
 app.use(express.json())
 app.use(cors())
-try{
-   
-    mongoose.connect(process.env.MONGO_URL)
-    console.log("connected to Database")
-}
-catch(error){
-    console.log("ERROR ", error)
 
-}
+mongoose.connect(process.env.MONGO_URL)
+.then(() => {
+    console.log("Connected to Database")
+})
+.catch((error) => {
+    console.log("MongoDB Error:", error)
+})
 
-app.use("/noteapp",noteroute)
+app.use("/noteapp", noteroute)
 
+const PORT = process.env.PORT || 3000
 
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
 
-
-app.listen(3000 , () => {
-  console.log("Server running on port 3000")
-})  
